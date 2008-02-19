e1719cf2cc6918d3639857a9fb0a6af1beefbb01
Shawn M Moore <sartak@gmail.com>
2008-02-19 02:30:43 -0500

    Initial import of autobox::Closure::Attributes

---
+++ lib/autobox/Closure/Attributes.pm
@@ -0,0 +1,83 @@
+#!perl
+package autobox::Closure::Attributes;
+use strict;
+use warnings;
+
+
+
+=head1 NAME
+
+autobox::Closure::Attributes - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use autobox::Closure::Attributes;
+    do_stuff();
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at gmail.com> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-autobox-closure-attributes at rt.cpan.org>, or browse
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=autobox-Closure-Attributes>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc autobox::Closure::Attributes
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/autobox-Closure-Attributes>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/autobox-Closure-Attributes>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=autobox-Closure-Attributes>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/autobox-Closure-Attributes>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
