c3388ad42faeeee686ba1e0ffdc9ece327cfb86a
Shawn M Moore <sartak@gmail.com>
2009-06-14 15:17:12 -0400

    Bump to 0.05

--- lib/autobox/Closure/Attributes.pm
+++ lib/autobox/Closure/Attributes.pm
@@ -2,7 +2,7 @@ package autobox::Closure::Attributes;
 use strict;
 use warnings;
 use base 'autobox';
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 sub import {
     shift->SUPER::import(CODE => 'autobox::Closure::Attributes::Methods');
